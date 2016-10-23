b6c98ed103842a2cef59afe308d003fdd4a8fa75
Yangyun Li <li19920212@gmail.com>
2016-10-23 18:21:24 -0500

    connect

--- server/server.js
+++ server/server.js
@@ -1,38 +1,41 @@
 var mongo = require('./mongo_connection')
 // Everything that was in the file before goes here
-var express = require('express');
+mongo.connect(function(err){
+  var express = require('express');
 
-app = express();
+  app = express();
 
-app.use(express.static('public')) // We will want this later
-app.set('view engine', 'ejs')
+  app.use(express.static('public')) // We will want this later
+  app.set('view engine', 'ejs')
 
-app.get('/spectator', function(req, res){
-      res.render('index')
-    })
-app.get('/', function(req, res){
+  app.get('/spectator', function(req, res){
+        res.render('index')
+      })
+  app.get('/', function(req, res){
 
-    })
+      })
 
-app.post('/hit', function(req,res){
-    res.json({
-      'success':  true,
-      'message':  '',
-    })
-})
+  app.post('/hit', function(req,res){
 
-app.post('/place_ship', function(req,res){
-    res.json({
-      'success':  true,
-    })
-})
+      res.json({
+        'success':  true,
+        'message':  '',
+      })
+  })
+
+  app.post('/place_ship', function(req,res){
+      res.json({
+        'success':  true,
+      })
+  })
 
 
-var port = process.env.PORT || 3000; // For when we deploy to Heroku
+  var port = process.env.PORT || 3000; // For when we deploy to Heroku
 
-var server = app.listen(port)
+  var server = app.listen(port)
 
-var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
+  var io = require('socket.io').listen(server);
+  io.sockets.on('connection', function (socket) {
 
-})
+  })
+});
